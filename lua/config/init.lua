ce806867fa219648a110f708b23cea9423ffbffc
Brad Rathke <brad@dahlialabs.ai>
2024-02-18 11:40:49 -0500

    feat: Clean up trailing whitespace on save

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,2 +1,11 @@
 require("config.settings")
 require("config.bootstrap")
+
+local PreWriteCleanup = vim.api.nvim_create_augroup("Cleanup", {})
+
+-- Trim trailing whitespace on save.
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+    group = PreWriteCleanup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
