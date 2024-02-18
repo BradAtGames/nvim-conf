d09795147b21db720b4b2ed5305fd581f35a3ff7
Brad Rathke <brad@dahlialabs.ai>
2024-02-18 07:25:26 -0500

    feat: Set up treesitter

---
+++ lua/config/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    -- Use a tag, typescript and json were broken at HEAD.
+    tag = "v0.9.2",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "c",
+                "cmake",
+                "cpp",
+                "glsl",
+                "go",
+                "gomod",
+                "gosum",
+                "gowork",
+                "graphql",
+                "hcl",
+                "hlsl",
+                "json",
+                "lua",
+                "make",
+                "proto",
+                "python",
+                "rust",
+                "terraform",
+                "toml",
+                "typescript",
+                "vhs",
+                "vimdoc",
+                "yaml",
+            },
+
+            indent = {
+                enable = true,
+            },
+
+            auto_install = false,
+        })
+    end
+}
