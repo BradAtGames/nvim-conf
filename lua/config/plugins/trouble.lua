e36c8612fb186c4cb790eb40b1826726e6349969
Brad Rathke <brad@dahlialabs.ai>
2024-02-18 22:01:04 -0500

    feat: Trouble diagnostics

---
+++ lua/config/plugins/trouble.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local trouble = require("trouble")
+		trouble.setup({})
+
+		vim.keymap.set("n", "<leader>tt", function()
+			trouble.toggle()
+		end)
+		vim.keymap.set("n", "<leader>tn", function()
+			trouble.next({ skip_groups = true, jump = true })
+		end)
+		vim.keymap.set("n", "<leader>tp", function()
+			trouble.prev({ skip_groups = true, jump = true })
+		end)
+	end,
+}
