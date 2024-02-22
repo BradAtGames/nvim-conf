30ad7c95cbb5ff5764412b1d2029f1cf6cdb8484
Brad Rathke <brad@dahlialabs.ai>
2024-02-22 07:38:39 -0500

    feat: prettier & prettierd

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -1,17 +1,18 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {},
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "black" },
-                go = { "goimports", "goimports-reviser", "golines", "gofumpt" }
-            },
-            format_on_save = {
-                timeout_ms = 1000,
-                lsp_fallback = true,
-            }
-        })
-    end,
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				go = { "goimports", "goimports-reviser", "golines", "gofumpt" },
+				typescript = { { "prettierd", "prettier" } },
+			},
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_fallback = true,
+			},
+		})
+	end,
 }
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,127 +1,120 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "dcampos/nvim-snippy",
-        "dcampos/cmp-snippy",
-        "jay-babu/mason-null-ls.nvim",
-        "nvimtools/none-ls.nvim",
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"dcampos/nvim-snippy",
+		"dcampos/cmp-snippy",
+		"jay-babu/mason-null-ls.nvim",
+		"nvimtools/none-ls.nvim",
+	},
 
-    config = function()
-        local c = require("cmp")
-        local cnl = require("cmp_nvim_lsp")
-        local caps = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cnl.default_capabilities())
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "ansiblels",
-                "clangd",
-                "cmake",
-                "dockerls",
-                "docker_compose_language_service",
-                "golangci_lint_ls",
-                "gopls",
-                "lua_ls",
-                "rust_analyzer",
-                "tsserver",
-            },
+	config = function()
+		local c = require("cmp")
+		local cnl = require("cmp_nvim_lsp")
+		local caps =
+			vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cnl.default_capabilities())
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"ansiblels",
+				"clangd",
+				"cmake",
+				"dockerls",
+				"docker_compose_language_service",
+				"golangci_lint_ls",
+				"gopls",
+				"lua_ls",
+				"rust_analyzer",
+				"tsserver",
+			},
 
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = caps
-                    })
-                end,
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = caps,
+					})
+				end,
 
-                ["lua_ls"] = function()
-                    local lsp = require("lspconfig")
-                    lsp.lua_ls.setup({
-                        capabilities = caps,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" }
-                                }
-                            }
-                        }
-                    })
-                end,
-            }
-        })
-        -- For mason supported stuff, that isn"t supported by mason-lspconfig.
-        require("mason-null-ls").setup({
-            ensure_installed = {
-                "black",
-                "golines",
-                "gofumpt",
-                "goimports",
-                "goimports-reviser",
-                -- "golangci-lint",
-                "gomodifytags",
-            },
-            handlers = {},
-        })
+				["lua_ls"] = function()
+					local lsp = require("lspconfig")
+					lsp.lua_ls.setup({
+						capabilities = caps,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
+		-- For mason supported stuff, that isn"t supported by mason-lspconfig.
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"black",
+				"golines",
+				"gofumpt",
+				"goimports",
+				"goimports-reviser",
+				"gomodifytags",
+				"prettier",
+				"prettierd",
+			},
+			handlers = {},
+		})
 
-        local selector = { behavior = c.SelectBehavior.Select }
-        c.setup({
-            snippet = {
-                expand = function(args)
-                    require("snippy").expand_snippet(args.body)
-                end
-            },
-            mapping = c.mapping.preset.insert({
-                ["<Enter>"] = c.mapping({
-                    i = function(fallback)
-                        if c.visible() and c.get_active_entry() then
-                            c.confirm({ behavior = c.ConfirmBehavior.Replace, select = false })
-                        elseif require("copilot.suggestion").is_visible() then
-                            require("copilot.suggestion").accept()
-                        else
-                            fallback()
-                        end
-                    end,
-                    s = c.mapping.confirm({ select = true }),
-                    c = c.mapping.confirm({ behavior = c.ConfirmBehavior.Replace, select = true }),
-                }),
-                ["<C-n>"] = c.mapping(
-                    function(fallback)
-                        if c.visible() then
-                            c.select_next_item({ behavior = selector })
-                        elseif require("copilot.suggestion").is_visible() then
-                            require("copilot.suggestion").next()
-                        else
-                            fallback()
-                        end
-                    end,
-                    { "i", "s", "c", }),
-                ["<C-p>"] = c.mapping(
-                    function(fallback)
-                        if c.visible() then
-                            c.select_prev_item({ behavior = selector })
-                        elseif require("copilot.suggestion").is_visible() then
-                            require("copilot.suggestion").prev()
-                        else
-                            fallback()
-                        end
-                    end,
-                    { "i", "s", "c", }),
-            }),
-            sources = c.config.sources({
-                { name = "nvim_lsp" },
-                { name = "snippy" },
-            })
-        })
-    end
-
+		local selector = { behavior = c.SelectBehavior.Select }
+		c.setup({
+			snippet = {
+				expand = function(args)
+					require("snippy").expand_snippet(args.body)
+				end,
+			},
+			mapping = c.mapping.preset.insert({
+				["<Enter>"] = c.mapping({
+					i = function(fallback)
+						if c.visible() and c.get_active_entry() then
+							c.confirm({ behavior = c.ConfirmBehavior.Replace, select = false })
+						elseif require("copilot.suggestion").is_visible() then
+							require("copilot.suggestion").accept()
+						else
+							fallback()
+						end
+					end,
+					s = c.mapping.confirm({ select = true }),
+					c = c.mapping.confirm({ behavior = c.ConfirmBehavior.Replace, select = true }),
+				}),
+				["<C-n>"] = c.mapping(function(fallback)
+					if c.visible() then
+						c.select_next_item({ behavior = selector })
+					elseif require("copilot.suggestion").is_visible() then
+						require("copilot.suggestion").next()
+					else
+						fallback()
+					end
+				end, { "i", "s", "c" }),
+				["<C-p>"] = c.mapping(function(fallback)
+					if c.visible() then
+						c.select_prev_item({ behavior = selector })
+					elseif require("copilot.suggestion").is_visible() then
+						require("copilot.suggestion").prev()
+					else
+						fallback()
+					end
+				end, { "i", "s", "c" }),
+			}),
+			sources = c.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "snippy" },
+			}),
+		})
+	end,
 }
